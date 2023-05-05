6f9ba6e5e86fe2504ea5f2ddc6e569d360170b77
zihadx <blankzihad@gmail.com>
2023-05-05 14:59:26 +0600

    make data for project

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const data = require('./chefes.json')
-const app = express()
+const express = require("express");
+const data = require("./chefes.json");
+const app = express();
 const port = 5000;
 
-app.get('/' , (req, res)=>{
-    res.send("my data information server is coming soon")
-})
-app.listen(port, ()=>{
-    console.log(`my server is running on port : ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("my data information server is coming soon");
+});
+
+app.listen(port, () => {
+  console.log(`my server is running on port : ${port}`);
+});
