a1b482ef042cf87bc57fa48d0f0999acf54eebce
zihadx <blankzihad@gmail.com>
2023-05-05 15:56:03 +0600

    get single data in local server

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
-const data = require("./chefes.json");
+const data = require("./chefs.json");
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("my data information server is coming soon");
+});
+
+// app.get("/data", (req, res) =>{
+//     res.send('data is coming')
+// })
+
+app.get("/data", (req, res) => {
+  res.send(data);
+});
+app.get("/data/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("i need data for id", id);
+  const singleData = data.find((singleData) => singleData.id === id) || {};
+  res.send(singleData)
 });
 
 app.listen(port, () => {
